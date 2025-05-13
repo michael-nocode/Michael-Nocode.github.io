c60ba191500d9bf76aba4490dfec362ec7a4c528
Michael-nocode <michael@nocode-testing.com>
2025-05-13 14:31:34 +0200

    correct config.js

--- TAIA/docusaurus.config.js
+++ TAIA/docusaurus.config.js
@@ -24,7 +24,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'michael-nocode', // Usually your GitHub org/user name.
   projectName: 'Taia Prompt Library', // Usually your repo name.
-  deploymentBranch : 'gh-pages'
+  deploymentBranch : 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
